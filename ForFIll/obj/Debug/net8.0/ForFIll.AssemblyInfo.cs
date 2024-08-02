58a402d909a2c15bb2fb55dbbc59be748bf68721
omar10120-admin <amr.dawoodi@hotmail.com>
2024-08-02 11:10:27 +0300

    update-8/2 asp2

--- ForFIll/obj/Debug/net8.0/ForFIll.AssemblyInfo.cs
+++ ForFIll/obj/Debug/net8.0/ForFIll.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForFIll")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+187d675093be34c4573eec890a0efadf1b33c4c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForFIll")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForFIll")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
