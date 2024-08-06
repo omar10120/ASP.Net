5ce69e5330e86949a0079e01446547f1034e2979
amr-d@joybox.corp <amr-d@tamayouz.corp>
2024-08-06 12:14:13 +0300

    edit fetchname

--- ForFIll/obj/Debug/net8.0/ForFIll.AssemblyInfo.cs
+++ ForFIll/obj/Debug/net8.0/ForFIll.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForFIll")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eba51d5f1044fea8af7deebf8762912fd474997")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad73aaab58d00bd7b37fafc24b816d770c745db")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForFIll")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForFIll")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
