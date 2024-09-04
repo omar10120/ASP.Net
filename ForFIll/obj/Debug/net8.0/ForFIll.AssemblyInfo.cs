afa40418310f0bdeb751b0cc8b9a22461669685a
omar10120-admin <amr.dawoodi@hotmail.com>
2024-09-04 20:59:24 +0300

    update two

--- ForFIll/obj/Debug/net8.0/ForFIll.AssemblyInfo.cs
+++ ForFIll/obj/Debug/net8.0/ForFIll.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForFIll")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09ff8fb9b12b21f143b5d3602b9d44dc3500cf37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ca0a1945c803633fadf6fe8bc39b8497b78560")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForFIll")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForFIll")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
